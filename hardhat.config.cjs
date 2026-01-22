96d8c0ddf9facca6d862755b8f271ea056494662
flavien <day.flavien@gmail.com>
2026-01-22 15:44:45 +0100

    FIrst Config HardHat

---
+++ hardhat.config.cjs
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
+
+module.exports = {
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.API_KEY}`,
+      accounts: {
+        mnemonic: process.env.PASS_PHRASE,
+      },
+    },
+  },
+  solidity: "0.8.28",
+};
